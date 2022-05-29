318ec60f04fabcdfef4dd7117b64b6e35cbd7ae2
Nathan King <nathanking3590@gmail.com>
2022-05-29 21:36:29 +0100

    remove old offset code

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -2,12 +2,6 @@ using UnityEngine;
 
 public class Player : MonoBehaviour, IMoveable
 {
-    [SerializeField]
-    private GameObject camOffset;
-
-    [SerializeField]
-    private float offsetX = .888f;
-
     private Animator animator;
     private SpriteRenderer sRenderer;
     private bool flipX;
@@ -16,18 +10,6 @@ public class Player : MonoBehaviour, IMoveable
         animator = GetComponent<Animator>();
         sRenderer = GetComponent<SpriteRenderer>();
         flipX = sRenderer.flipX;
-    }
-
-    void FixedUpdate() {
-        if (flipX != sRenderer.flipX) {
-            camOffset.transform.position = new Vector3(
-                flipX ? transform.position.x + offsetX : transform.position.x - offsetX,
-                camOffset.transform.position.y,
-                camOffset.transform.position.z
-            );
-
-            flipX = sRenderer.flipX;
-        }
     }
 
     public bool CanWalk() {
